6bab40d89f9f748288c578609e536b9ab0517aca
Сергей <big_p1g@mail.ru>
2026-02-16 11:28:49 +0300

    НАЧАЛО ЦАП

---
+++ get-dac/r2r_dac.py
@@ -0,0 +1,42 @@
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+class R2R_DAC:
+    def __init__(self, gpio_bits, dynamic_range, verbose = False):
+        self.gpio_bits = gpio_bits
+        self.dynamic_range = dynamic_range
+        self.verbose = verbose
+
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.gpio_bits, GPIO.OUT, initial = 0)
+
+    def deinit(self):
+        GPIO.output(self.gpio_bits, 0)
+        GPIO.cleanup()
+    
+    def set_number(self, number):
+        print(number)
+        number = [int(element) for element in bin(number)[2:].zfill(8)]
+        GPIO.output(self.gpio_bits, number)
+        print(number)
+    
+    def set_voltage(self, votlage):
+        if not (0.0 <= voltage <= self.dynamic_range):
+            print(f"Напряжение выходит за динамический диапазон ЦАП (0.00 - {self.dynamic_range:.2f} В)")
+            print("Устанавливаем 0.0 В")
+            return 0
+        self.set_number(int(voltage / self.dynamic_range * 255))
+
+if __name__ == "__main__":
+    try:
+        dac = R2R_DAC([16, 20, 21, 25, 26, 17, 27, 22], float(3.146), True)
+
+        while True:
+            try:
+                voltage = float(input("Введите напряжение в Вольтах: "))
+                dac.set_voltage(voltage)
+
+            except ValueError:
+                print("Вы ввели на число. Попробуйте ещё раз\n")
+
+    finally:
+        dac.deinit()
